91d8fd3e2a0021ba8c7264a76598b7554baac745
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-07-11 08:15:47 -0700

    prep 06 complete

---
+++ lighthouse-prep/06.js
@@ -0,0 +1,58 @@
+const whereCanIPark = (spots, vehicle) => {
+  let spotType = [];
+  let result = [];
+
+  if (vehicle === 'regular') {
+    spotType = ['R'];
+  } else if (vehicle === 'small') {
+    spotType = ['S', 'R']
+  } else spotType = ['S', 'R', 'M']
+
+  spots.forEach((row, rowIndex) => {
+    row.forEach((spot, spotIndex) => {
+      if (spotType.includes(spot)) {
+        result.push([rowIndex, spotIndex])
+      }
+    })
+  })
+
+  if (!result.length) result = [false]
+
+  return result[0];
+}
+
+console.log(whereCanIPark(
+  [
+    // COLUMNS ARE X
+    // 0    1    2    3    4    5
+    ['s', 's', 's', 'S', 'R', 'M'], // 0 ROWS ARE Y
+    ['s', 'M', 's', 'S', 'r', 'M'], // 1
+    ['s', 'M', 's', 'S', 'r', 'm'], // 2
+    ['S', 'r', 's', 'm', 'r', 'M'], // 3
+    ['S', 'r', 's', 'm', 'r', 'M'], // 4
+    ['S', 'r', 'S', 'M', 'M', 'S']  // 5
+  ],
+  'regular'
+)); // [4, 0]
+
+console.log(whereCanIPark(
+  [
+    ['M', 'M', 'M', 'M'],
+    ['M', 's', 'M', 'M'],
+    ['M', 'M', 'M', 'M'],
+    ['M', 'M', 'r', 'M']
+  ],
+  'small'
+)); // false
+
+console.log(whereCanIPark(
+  [
+    ['s', 's', 's', 's', 's', 's'],
+    ['s', 'm', 's', 'S', 'r', 's'],
+    ['s', 'm', 's', 'S', 'r', 's'],
+    ['S', 'r', 's', 'm', 'r', 's'],
+    ['S', 'r', 's', 'm', 'R', 's'],
+    ['S', 'r', 'S', 'M', 'm', 'S']
+  ],
+  'motorcycle'
+)) // [3, 1]

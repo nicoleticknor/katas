42b094e2e700079c1c60f17ab3f96b72e3f5c995
Nicole Ticknor <36491978+nicoleticknor@users.noreply.github.com>
2020-07-14 07:29:09 -0700

    euler 4 wip

--- euler/4.js
+++ euler/4.js
@@ -1,21 +1,21 @@
-const sumOfSquares = (num) => {
-  let result = 0;
-  for (let i = 1; i <= num; i++) {
-    result += Math.pow(i, 2);
+const palindromer = () => {
+  let first;
+  let last;
+  let prod;
+  let result;
+  for (let i = 1; i < 100; i++) {
+    for (let j = 1; j < 100; j++) {
+      prod = (i * j).toString();
+      if (prod.length % 2 === 0) {
+        first = prod.slice(0, prod.length / 2).split("").join();
+        last = prod.slice(prod.length / 2).split("").reverse().join();
+        if (first === last) {
+          result = prod;
+        }
+      }
+    }
   }
   return result;
 };
 
-const squareOfSums = (num) => {
-  let result = 0;
-  for (let i = 1; i <= num; i++) {
-    result += i;
-  }
-  result = Math.pow(result, 2);
-  return result;
-};
-
-const sOSq = sumOfSquares(100);
-const sqOS = squareOfSums(100);
-
-console.log(sqOS - sOSq);
+console.log(palindromer());
